db580e24bc71e997665af1caf9ea6a1800fa6b38
user DONGKWON <pongdong2323@gmail.com>
2021-11-03 15:29:55 +0900

    랜덤홀짝

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
-x=5
+import random
 
-if(x%2==1):
-    print("홀수입니다.")
+a=random.randrange(1,11)
+print("구슬갯수:",a)
+
+my =input("홀or짝 choice: ")
+print(my)
+
+dab=""
+if a%2==0:
+    print("짝")
+    dab ="짝"
 else:
-    print("짝수입니다.")
-    print("짝수입니다.")
+    print("홀")
+    dab ="홀"
+
+if my == dab:
+    print("맞다")
+else:
+    print("빵")
