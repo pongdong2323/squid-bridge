cf4db1da90cfbd1e0c7366a6538184c5fb615255
user DONGKWON <pongdong2323@gmail.com>
2021-11-03 17:40:36 +0900

    기회2번

--- hello.py
+++ hello.py
@@ -3,19 +3,22 @@ import random
 a=random.randrange(1,11)
 print("구슬갯수:",a)
 
-my =input("홀or짝 choice: ")
-print(my)
+for i in range(2):
+    my = input("홀or짝 choice:  ")
 
-dab=""
-if a%2==0:
-    print("짝")
-    dab ="짝"
-else:
-    print("홀")
-    dab ="홀"
+    if my == "홀" or my == "짝":
+        dab = ""
+        if a % 2 == 0:
+            dab = "짝"
+        else:
+            dab = "홀"
 
-if my == dab:
-    print("맞다")
-else:
-    print("빵")
-    print("QKd")
+        if my == dab:
+            print("딩동댕")
+            break
+        
+        else:
+            print("땡")
+
+    else:
+        print("다시 입력")
