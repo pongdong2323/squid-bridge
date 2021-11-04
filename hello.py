6aba01f2f8dd472bb3becc211538c40c97049178
user DONGKWON <pongdong2323@gmail.com>
2021-11-04 13:37:54 +0900

    반복

--- hello.py
+++ hello.py
@@ -2,12 +2,22 @@ import random
 
 a=random.randrange(1,11)
 print("오징어 게임을 시작하겠습니다.")
-print("구슬갯수:{}".format(a))
+print("내기 구슬갯수:{}".format(a))
 my_marble = 10
 
-for i in range(2):
+for i in range(50):
     my = input("홀or짝 choice하세요:  ")
-    betting = int(input("베팅할 구슬갯수를 입력하세요: "))
+    try:
+        # 구슬의 갯수 베팅
+        betting = int(input("구슬 몇 개 베팅? "))
+    except:
+        print("숫자만 입력해라~")
+        continue
+
+
+    if my_marble < betting:
+        print("구슬 모자름요. {}개 안에서 베팅하세요.".format(my_marble))
+        continue
 
     if my == "홀" or my == "짝":
         dab = ""
@@ -20,12 +30,17 @@ for i in range(2):
             print("내꺼")
             my_marble = my_marble + betting
             print("내구슬 갯수: {}".format(my_marble))
-            break
+            continue
         
         else:
             print("옛다")
             my_marble = my_marble - betting
             print("내구슬 갯수: {}".format(my_marble))
+            continue
+
+        if my_marble == 0 or my_marble == 20:
+            print("끝~")
             break
+
     else:
         print("다시 입력")
