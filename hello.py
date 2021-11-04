a69df77aa8126e3ad7bd472c7c6d7a1f8a696941
user DONGKWON <pongdong2323@gmail.com>
2021-11-04 14:02:15 +0900

    홀짝 예외처리

--- hello.py
+++ hello.py
@@ -6,9 +6,9 @@ print("내기 구슬갯수:{}".format(a))
 my_marble = 10
 
 for i in range(50):
-    my = input("홀or짝 choice하세요:  ")
     try:
         # 구슬의 갯수 베팅
+        my = input("홀or짝 choice하세요:  ")
         betting = int(input("구슬 몇 개 베팅? "))
     except:
         print("숫자만 입력해라~")
@@ -30,15 +30,15 @@ for i in range(50):
             print("내꺼")
             my_marble = my_marble + betting
             print("내구슬 갯수: {}".format(my_marble))
-            continue
+            
         
         else:
             print("옛다")
             my_marble = my_marble - betting
             print("내구슬 갯수: {}".format(my_marble))
-            continue
+            
 
-        if my_marble == 0 or my_marble == 20:
+        if my_marble == 0 or my_marble >= 20:
             print("끝~")
             break
 
