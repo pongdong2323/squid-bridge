96451368cb3c959329beefbd162d4a306d7c31ba
user DONGKWON <pongdong2323@gmail.com>
2021-11-04 11:33:44 +0900

    구슬 베팅

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 import random
 
 a=random.randrange(1,11)
-print("구슬갯수:",a)
+print("오징어 게임을 시작하겠습니다.")
+print("구슬갯수:{}".format(a))
+my_marble = 10
 
 for i in range(2):
-    my = input("홀or짝 choice:  ")
+    my = input("홀or짝 choice하세요:  ")
+    betting = int(input("베팅할 구슬갯수를 입력하세요: "))
 
     if my == "홀" or my == "짝":
         dab = ""
@@ -14,11 +17,15 @@ for i in range(2):
             dab = "홀"
 
         if my == dab:
-            print("딩동댕")
+            print("내꺼")
+            my_marble = my_marble + betting
+            print("내구슬 갯수: {}".format(my_marble))
             break
         
         else:
-            print("땡")
-
+            print("옛다")
+            my_marble = my_marble - betting
+            print("내구슬 갯수: {}".format(my_marble))
+            break
     else:
         print("다시 입력")
